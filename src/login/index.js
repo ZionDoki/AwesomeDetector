7d2883bd0af1400df917bcbaf79b7540c9a47741
YYuan_H <1079312238@qq.com>
2019-09-20 21:00:35 +0800

    修改登录逻辑

--- src/login/index.js
+++ src/login/index.js
@@ -1,7 +1,126 @@
 import React from 'react';
+import { FormControlLabel, Checkbox } from '@material-ui/core';
+import { makeStyles, Container, Avatar, Typography, TextField, Button, InputAdornment, IconButton } from '@material-ui/core';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Lock from '@material-ui/icons/Lock';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+
+
+const useStyles = makeStyles(theme => ({
+    box: {
+        marginTop: theme.spacing(15),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        marginBottom: theme.spacing(2),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    button: {
+        marginTop: theme.spacing(3),
+    },
+}));
 
 export default function Login(props) {
+    const classes = useStyles();
+    const { setKey, history } = props;
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [values, setValues] = React.useState({
+        username: '',
+        password: '',
+        remember: false,
+    });
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+    const handleChange = name => event => {
+        if(name === 'remember')
+            setValues({ ...values, [name]: event.target.checked });
+        else setValues({ ...values, [name]: event.target.value });
+    };
+    const handleSubmit = event => {
+        {/* 提交表单 */}
+        setKey(values.password, values.remember);
+        history.push('/dashboard');
+    };
+
     return (
-        <p>Login</p>
+        <Container maxWidth='xs'>
+            <div className={classes.box}>
+                <Avatar className={classes.avatar}>
+                    <AccountBoxIcon />
+                </Avatar>
+                <Typography variant='h5' gutterBottom>登录</Typography>
+                <form onSubmit={handleSubmit}>
+                    <TextField 
+                        required
+                        autoFocus
+                        id='username'
+                        label='用户名'
+                        name='username'
+                        fullWidth
+                        variant='outlined'
+                        margin='normal'
+                        value={values.username}
+                        onChange={handleChange('username')}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <AccountCircle />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <TextField 
+                        required
+                        id='password'
+                        label='密码'
+                        name='password'
+                        fullWidth
+                        variant='outlined'
+                        margin='normal'
+                        type={showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Lock />
+                                </InputAdornment>
+                            ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        // edge='end'
+                                        onClick={handleClickShowPassword}
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value='remember' color='primary' checked={values.remember} onChange={handleChange('remember')} />}
+                        // control={<Checkbox value='remember' color='primary'  onChange={handleChange('remember')} />}
+                        label='Remember me'
+                    />
+                    <Button 
+                        type='submit'
+                        size='large'
+                        fullWidth 
+                        variant='contained' 
+                        color='primary' 
+                        className={classes.button}
+                    >
+                        登录
+                    </Button>
+                </form>
+            </div>
+        </Container>
     );
 }
