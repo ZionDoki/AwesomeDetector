ecc6d9f06836da7b154a351ed46d91aba93cd7b0
YYuan_H <1079312238@qq.com>
2019-12-26 11:09:17 +0800

    删除无用的文件引入

--- src/dashboard/speedTest/downloadChart/index.js
+++ src/dashboard/speedTest/downloadChart/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, Paper, Typography } from '@material-ui/core';
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Label, Tooltip, Legend } from 'recharts';
 
 import MyLine from '../../../component/myline';
 
