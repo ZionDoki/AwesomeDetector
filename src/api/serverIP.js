c21140af58983e3dde12f407eed5c4b4c6ee6b81
YYuanH <1079312238@qq.com>
2020-05-02 15:22:41 +0800

    更改IP地址

--- src/api/serverIP.js
+++ src/api/serverIP.js
@@ -1,3 +1,3 @@
 // export const testIP = "http://172.16.44.31:3000";
-export const testIP = "http://202.201.1.130:8080";
+export const testIP = "http://www.zion-moe.com:3000";
 export const test = true;
